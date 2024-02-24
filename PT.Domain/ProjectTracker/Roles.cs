08a416975a62f1855924fa6dd4566c679699fb91
Reign-Beaux <samc920130@hotmail.com>
2024-02-24 01:54:22 -0600

    Se crearon cositas nuevas

---
+++ PT.Domain/ProjectTracker/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace PT.Domain.ProjectTracker
+{
+    public class Roles
+    {
+        public int Id { get; set; }
+        public string? Code { get; set; }
+        public string? Description { get; set; }
+    }
+}
