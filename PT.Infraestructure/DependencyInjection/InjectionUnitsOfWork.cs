b70ca03faead91cd0766b2ee43fac401449b85d3
Reign-Beaux <samc920130@hotmail.com>
2024-02-24 14:24:05 -0600

    se actualizaron los units of work

---
+++ PT.Infraestructure/DependencyInjection/InjectionUnitsOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PT.Infraestructure.Persistence.ProjectTracker.UnitOfWorkProjectTracker;
+
+namespace PT.Infraestructure.DependencyInjection
+{
+    public static class InjectionUnitsOfWork
+    {
+        public static IServiceCollection AddInjectionUnitsOfWork(this IServiceCollection services)
+        {
+            services.AddTransient<IUnitOfWorkProjectTracker, UnitOfWorkProjectTracker>();
+
+            return services;
+        }
+    }
+}
