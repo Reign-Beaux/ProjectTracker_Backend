4958fac5535bfe935d558b0e4aec7e2359dc2b9b
Reign-Beaux <samc920130@hotmail.com>
2024-02-24 16:26:16 -0600

    Se agregp´ó inyección de dependencias

---
+++ PT.Application/DependencyInjection/FeaturesInjection.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace PT.Application.DependencyInjection
+{
+    public static class FeaturesInjection
+    {
+        public static IServiceCollection AddFeaturesServices(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            services.AddMediatR(assembly);
+
+            return services;
+        }
+    }
+}
