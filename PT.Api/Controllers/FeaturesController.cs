0ccee99dde37f8dda15c90cc7379472bab1cc1d5
Reign-Beaux <samc920130@hotmail.com>
2024-03-01 22:47:18 -0600

    se agregaron los endpoints

---
+++ PT.Api/Controllers/FeaturesController.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PT.Application.Features.Features.Commands.FeatureDelete;
+using PT.Application.Features.Features.Commands.FeatureInsert;
+using PT.Application.Features.Features.Commands.FeatureUpdate;
+using PT.Application.Features.Features.Queries.FeatureGetAll;
+using PT.Application.Features.Features.Queries.FeatureGetById;
+
+namespace PT.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : BaseController
+    {
+        public FeaturesController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var request = new FeatureGetAllQuery();
+            var response = await _mediator.Send(request);
+            return HandleResponse(response);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var request = new FeatureGetByIdQuery(id);
+            var response = await _mediator.Send(request);
+            return HandleResponse(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert([FromBody] FeatureInsertCommand request)
+        {
+            var response = await _mediator.Send(request);
+            return HandleResponse(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] FeatureUpdateCommand request)
+        {
+            var response = await _mediator.Send(request);
+            return HandleResponse(response);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var request = new FeatureDeleteCommand(id);
+            var response = await _mediator.Send(request);
+            return HandleResponse(response);
+        }
+    }
+}
