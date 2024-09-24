577946048664c9806c36cc461ee3492dd066e5cb
Reign-Beaux <samc920130@hotmail.com>
2024-09-23 21:21:59 -0600

    Se agregaron las relaciones entre capas

---
+++ Web.API/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Web.API.Middlewares;
+
+namespace Web.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAPI(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddTransient<GlobalExceptionHandlingMiddleware>();
+
+            return services;
+        }
+    }
+}
